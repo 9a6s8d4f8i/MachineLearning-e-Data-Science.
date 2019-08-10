fec66d37d53b59927f514b0204ef7096f994f9a3
9a6s8d4f8i <felipemolinari874@gmail.com>
2019-08-10 08:21:10 -0300

    Ajustes finais Classificação

--- Resultados/resultados_credit_data.py
+++ Resultados/resultados_credit_data.py
@@ -3,13 +3,18 @@
 # =============================================================================  
 # 
 # 0.872 Base line Classifier #Os algoritmos precisam ser maior que esse valor
-# 
+ 
 # 0.938 - Naive bayes com ( inconsistência + faltantes + escalonamento)
-# 0.982 - Arvore de decisão com ( inconsistência + faltantes + escalonamento )    
+
+# 0.982 - Arvore de decisão com ( inconsistência + faltantes + escalonamento )  
+
 # 0.984 - Random forest com n_estimators = 40 ( inconsistência + faltantes + escalonamento )  
+
 # 0.836 - KNN com k = 5 sem o escalonamento
 # 0.986 - Knn com k = 5 com escalonamento
+
 # 0.9439 - Regressão Logística com ( inconsistência + faltantes + escalonamento)
+
 # 0.996 - Redes neurais utilizando sklearn
 # 0.998 - Redes neurais utilizando keras
 # =============================================================================
